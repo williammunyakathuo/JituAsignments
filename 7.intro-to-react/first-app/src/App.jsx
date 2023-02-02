b7183ed7a6bc610f6cad78c8c6e68e38af9ea5c8
williammumyakathuo <tjwellieyam@gamil.com>
2023-02-02 10:16:33 +0300

    the jitu

--- 7.intro-to-react/first-app/src/App.jsx
+++ 7.intro-to-react/first-app/src/App.jsx
@@ -5,56 +5,66 @@ function App() {
 const [items, setItems]= useState(
   [{
     id: 1,
-    todo: "do asignments",
+    todo: "Do asignments",
     date: "7/4/2022"
   }, {
     id: 2,
-    todo: "wash clothes",
+    todo: "Do laundry",
     date: "7/14/2022"
   }, {
     id: 3,
-    todo: "wash clothes",
+    todo: "Attend Lecture",
     date: "5/27/2022"
   }, {
     id: 4,
-    todo: "code",
+    todo: "Read a novel",
     date: "3/4/2022"
   }, {
     id: 5,
-    todo: "write weekly report",
+    todo: "Write weekly report",
     date: "12/2/2022"
   }, {
     id: 6,
-    todo: "do asignments",
+    todo: "Visit cousin",
     date: "3/5/2022"
   }, {
     id: 7,
-    todo: "code",
+    todo: "Go shopping",
     date: "9/24/2022"
   }, {
     id: 8,
-    todo: "code",
+    todo: "Send Email",
     date: "2/27/2022"
   }, {
     id: 9,
-    todo: "write weekly report",
+    todo: "Order imports",
     date: "11/1/2022"
   }, {
     id: 10,
-    todo: "code",
+    todo: "Write code",
     date: "7/26/2022"
   }]  
 )
 
   return (
     <div className="App">
-      <h1>TO-DO LIST</h1>
-      {items.map((item)=>
-        <div className="itempreview" key={item.id} >
-          <h2>{item.todo}</h2>
-          <p>To be done by {item.date}</p>
+      <div className="container"></div>
+        <div className="header"> 
+          <h1>TO-DO LIST</h1>
+          <h2>Add to your list here</h2>
+          <div className='addnew'>
+            <input className='add' type="text"  placeholder='Add new to do'/>
+            <button>Add New</button>
+          </div>
         </div>
-      )}
+        <div className="todolist">
+          {items.map((item)=>
+            <div className="itempreview" key={item.id} >
+              <h2>{item.todo}</h2>
+              <p>To be done by {item.date}</p>
+            </div>
+          )}
+        </div>  
     </div>
   );
 }
